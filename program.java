43ac8079e0ed16b07e62c63f3bb08198c0776ad4
Valerii_Panov <val250640@mail.ru>
2023-02-12 17:47:22 +1000

    Start

---
+++ program.java
@@ -0,0 +1,16 @@
+
+public class program {
+    public static void main(String[] args) {
+        String = "aaaabbbcdd";
+        
+    }
+
+    static String rar(String line){
+        StringBuilder res = new StringBuilder();
+        for (int i = 0; i < line.length; i++){
+            char sym = i
+        }
+
+        return res;
+    }
+}
